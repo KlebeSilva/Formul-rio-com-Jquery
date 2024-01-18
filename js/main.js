70f2a746202fdfabcebae7e20978c8501f2d3375
kleber silva <kleberfaneco73@gmail.com>
2024-01-18 01:15:33 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#cpf').mask('000.000.000-00', {
+        placeholder:'ex: 000.000.000-00'
+    })
+
+    $('#cep').mask('00000-000', {
+        placeholder:'ex: 00000-00'
+    })
+})
