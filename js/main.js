055466cd7910282a6418ef28bedd6c8773bb4026
kleber silva <kleberfaneco73@gmail.com>
2024-01-19 10:29:32 -0300

    mudança no placeholder do cep

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         placeholder:'CPF: 000.000.000-00'
     })
     $('#cep').mask('00000-000', {
-        placeholder:'CEP (ex: 00000-00)'
+        placeholder:'CEP: 00000-00'
     })
     $('#telefone').mask('(00) 00000-0000',{
         placeholder:'Celular:(99) 99999-9999 '
